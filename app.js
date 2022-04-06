0ff4264f2c39491694dcae6f85d3d6d17536ec46
Ethan Mai <iam.ethanmai@gmail.com>
2022-04-06 09:25:37 -0400

    animated gradient

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ function sendMail() {
 }
 
 
-particles.particlesJS.load('particles-js', 'particles.json', function() {
-    console.log('callback - particles.js config loaded');
+
+/*
+var myNav = document.getElementById('navbar');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
     }
-);
-
-//particles js
-
+};
+*/
