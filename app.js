1623eaf19ab1269c22c9678c7349a61196a6701b
Ethan Mai <iam.ethanmai@gmail.com>
2022-04-06 14:30:09 -0400

    language button

--- app.js
+++ app.js
@@ -2,20 +2,3 @@
 function sendMail() {
     return location.href = "mailto:iam.ethanmai@gmail.com";
 }
-
-
-
-/*
-var myNav = document.getElementById('navbar');
-window.onscroll = function () { 
-    "use strict";
-    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-transparent");
-    } 
-    else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
-    }
-};
-*/
